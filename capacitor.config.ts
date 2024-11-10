2c4b426b354ab09efaaec3607b411b771df95ef8
Juan Morales <jmorales@utriasystems.com>
2024-11-09 19:50:49 -0500

    Versión inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'gestor-de-tareas',
+  webDir: 'dist/gestor-de-tareas/browser',
+  bundledWebRuntime: false
+};
+
+export default config;
